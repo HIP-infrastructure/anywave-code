a41edee83a35d4a302822c6b7006d43d4ba5c47d
Bruno Colombet <bruno.colombet@univ-amu.fr>
2018-03-23 14:55:53 +0100

    Fixed a bug with GUIProcess that must implement run(const QStringList& args)

--- include/AwProcessInterface.h
+++ include/AwProcessInterface.h
@@ -169,7 +169,8 @@ public:
 
 	/** Register a new AwProcessGUIWidget that will close/kill the process when the user closes the widget **/
 	void registerGUIWidget(AwProcessGUIWidget *widget);
-	virtual void run(const QStringList& args = QStringList()) { run();  }
+	/* Implement this method in the derived object. */
+	virtual void run(const QStringList& args = QStringList()) {  }
 
 public slots:
 	void stop() { emit aboutToBeDestroyed(); m_plugin->deleteInstance(this); }
