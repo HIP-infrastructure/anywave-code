405162036d289b4b63d2e31f6a9c8f4b5ed74957
bcolombet <Bruno.Colombet@univ-amu.fr>
2018-10-05 17:29:07 +0200

    Linux: fixes CMAKE building.

--- resources/linux/start_aw.sh
+++ resources/linux/start_aw.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
-
-if [ -f $HOME/AnyWave/matlab.sh ];
-then
-	echo Executing matlab.sh
-	. $HOME/AnyWave/matlab.sh
-fi
-
-export QT_QPA_PLATFORM_PLUGIN_PATH=$SCRIPTPATH/lib/plugins/platforms
-
 if [ -f $HOME/AnyWave/matlab.sh ];
 then
 	chmod +x $HOME/AnyWave/matlab.sh
