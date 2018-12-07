6ed5094f18d5e814b373dace3eaef25c222cb827
Bruno Colombet <bruno.colombet@univ-amu.fr>
2018-12-07 16:56:29 +0100

    New command line parser

---
+++ include/AwCommandLine.h
@@ -0,0 +1,8 @@
+#pragma once
+//#include <QPair>
+#include <QHash>
+#include <QString>
+#include <QVariant>
+// command line parsing
+//using AwArgument = QPair<QString, QString>;
+using AwArguments = QHash<QString, QVariant>;
