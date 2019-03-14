82cb6fd307acc6a7c053a0420a7ce045a941227a
Bruno Colombet <bruno.colombet@univ-amu.fr>
2019-03-14 15:31:07 +0100

    Minor changes

--- src/Filtering/AwFilterSettings.cpp
+++ src/Filtering/AwFilterSettings.cpp
@@ -274,7 +274,6 @@ void AwFilterSettings::load(const QString& path)
 	}
 	m_hash.clear();
 	// types that can be filtered:
-//	QStringList types = { "EEG", "SEEG", "EMG", "MEG", "GRAD", "ICA", "Source", "Reference", "ECG" };
 	// all types of channels can be filtered
 	auto types = AwChannel::types();
 	QJsonObject root = doc.object();
