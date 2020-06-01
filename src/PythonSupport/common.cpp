e0bd383edc19d58c44eedd93933e6a2c43f3b582
Bruno Colombet <bruno.colombet@univ-amu.fr>
2020-06-01 20:41:08 +0200

    Update common.cpp

--- src/PythonSupport/common.cpp
+++ src/PythonSupport/common.cpp
@@ -1,8 +1,8 @@
 /////////////////////////////////////////////////////////////////////////////////////////
 // 
-//                 Université d’Aix Marseille (AMU) - 
-//                 Institut National de la Santé et de la Recherche Médicale (INSERM)
-//                 Copyright © 2013 AMU, INSERM
+//                 Universit dAix Marseille (AMU) - 
+//                 Institut National de la Sant et de la Recherche Mdicale (INSERM)
+//                 Copyright  2013 AMU, INSERM
 // 
 //  This library is free software; you can redistribute it and/or
 //  modify it under the terms of the GNU Lesser General Public
@@ -20,7 +20,7 @@
 //
 //
 //
-//    Author: Bruno Colombet – Laboratoire UMR INS INSERM 1106 - Bruno.Colombet@univ-amu.fr
+//    Author: Bruno Colombet  Laboratoire UMR INS INSERM 1106 - Bruno.Colombet@univ-amu.fr
 //
 //////////////////////////////////////////////////////////////////////////////////////////
 
@@ -218,7 +218,7 @@ void TCPRequest::clear()
 	m_data.clear();
 }
 
-bool TCPRequest::sendRequest(QString& jsonString)
+bool TCPRequest::sendRequest(const QString& jsonString)
 {
 	if (m_status != TCPRequest::connected) {
 		PyErr_SetString(AnyWaveError, "Error while sending request to AnyWave: not connected.");
