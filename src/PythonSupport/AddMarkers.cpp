7e019d66904fa5100ee5c520857119d4d2ea2469
brunoc <bruno.colombet@univ-amu.fr>
2020-06-02 18:32:41 +0200

    Linux

--- src/PythonSupport/AddMarkers.cpp
+++ src/PythonSupport/AddMarkers.cpp
@@ -108,7 +108,7 @@ PyObject *send_markers(const AwMarkerList& markers)
         stream << marker->targetChannels();
 		stream << marker->color();
     }
-	if (!request.sendRequest()) 
+	if (!request.sendRequest(QString()) 
 			return NULL;
 	return Py_None;
 }
