03357e17636db82dd76ef563b16701c023513abc
Bruno Colombet <bruno.colombet@univ-amu.fr>
2020-06-03 09:22:02 +0200

    Update GetMarkers.cpp

--- src/PythonSupport/GetMarkers.cpp
+++ src/PythonSupport/GetMarkers.cpp
@@ -1,8 +1,8 @@
 /////////////////////////////////////////////////////////////////////////////////////////
 // 
-//                 Université d’Aix Marseille (AMU) - 
-//                 Institut National de la Santé et de la Recherche Médicale (INSERM)
-//                 Copyright © 2013 AMU, INSERM
+//                 Universit dAix Marseille (AMU) - 
+//                 Institut National de la Sant et de la Recherche Mdicale (INSERM)
+//                 Copyright  2013 AMU, INSERM
 // 
 //  This library is free software; you can redistribute it and/or
 //  modify it under the terms of the GNU Lesser General Public
@@ -20,7 +20,7 @@
 //
 //
 //
-//    Author: Bruno Colombet – Laboratoire UMR INS INSERM 1106 - Bruno.Colombet@univ-amu.fr
+//    Author: Bruno Colombet  Laboratoire UMR INS INSERM 1106 - Bruno.Colombet@univ-amu.fr
 //
 //////////////////////////////////////////////////////////////////////////////////////////
 
@@ -46,7 +46,7 @@ PyObject *request_markers(const QString& file, const QString& extractTriggers, Q
 
 
 	stream_data << file << extractTriggers << values << labels << channels;
-	if (!request.sendRequest()) {
+	if (!request.sendSimpleRequest()) {
 		return NULL;
 	}
 	if (!request.getResponse()) {
