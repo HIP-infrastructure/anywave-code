25c8a5b89c77f257dec9ec61616ee2cb686e31bd
bruno colombet <bruno.colombet@univ-amu.fr>
2021-05-31 14:57:34 +0200

    Mac

--- resources/macx/make_pkg.sh
+++ resources/macx/make_pkg.sh
@@ -1,9 +1,10 @@
  #!/bin/bash
-mv ./AnyWave.app/Contents/Plugins/libDeltamedReader.dylib .
 
 APP_DIR=/Users/bruno/dev/anywave-mac/build/AnyWave.app
 ID="Apple Development: thechewie13@gmail.com (MY3J8AN9H6)"
 source ./sign.sh
+
+mv ./AnyWave.app/Contents/Plugins/libDeltamedReader.dylib .
 
 sudo pkgbuild --install-location /Applications --identifier fr.univ-amu.ins.anywave --version 1 \
   --scripts ./scripts --component ./AnyWave.app ./AnyWave.pkg
@@ -11,4 +12,12 @@ sudo pkgbuild --install-location /Applications --identifier fr.univ-amu.ins.anyw
 sudo codesign -s "$ID" ./AnyWave.pkg
 
 sudo hdiutil create -volname AnyWave -srcfolder ./AnyWave.pkg -ov -format UDZO ./AnyWave.dmg
-codesign -s "$ID" ./AnyWave.dmg
+sudo codesign -s "$ID" ./AnyWave.dmg
+
+mv ./libDeltamedReader.dylib ./AnyWave.app/Contents/Plugins
+
+sudo pkgbuild --install-location /Applications --identifier fr.univ-amu.ins.anywave --version 1 \
+  --scripts ./scripts --component ./AnyWave.app ./AnyWaveINS-$1.pkg
+
+sudo hdiutil create -volname AnyWave -srcfolder ./AnyWaveINS-$1.pkg -ov -format UDZO ./AnyWaveINS-$1.dmg
+sudo codesign -s "$ID" ./AnyWaveINS-$1.dmg
