d3c4190950a7bdd91a0bdf63499d4cd4afda00cb
Bruno Colombet <bruno.colombet@univ-amu.fr>
2021-06-01 14:08:57 +0200

    Python API is now a independent project

--- src/Python/anywave/init.py
+++
@@ -1,7 +0,0 @@
-import anywave
-
-
-def init(argv):
-    _, pid, port = argv
-    anywave.pid = int(pid)
-    anywave.port = int(port)
--- src/Python/anywave/network/constants.py
+++
@@ -1,8 +0,0 @@
-"""
-"""
-
-SIZE_INT = 4
-
-# States for tcp connections
-STATUS_SUCCESS = 0
-STATUS_FAILED  = 1
