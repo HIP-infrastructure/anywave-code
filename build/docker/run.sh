9d548b955f3dfcce0f70dcd521872cf5e1a05f51
Bruno Colombet <bruno.colombet@univ-amu.fr>
2022-02-28 18:45:00 +0100

    Linux docker build/run update

--- build/docker/run.sh
+++ build/docker/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+image=$1
 export UID=$(id -u)
 export GID=$(id -g)
 
@@ -11,4 +11,4 @@ fi
 docker run -it -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /mnt:/mnt -v /home:/home  \
      --user $UID:$GID --workdir="/home/$USER" --volume="/etc/group:/etc/group:ro" \
      --volume="/etc/passwd:/etc/passwd:ro" --volume="/etc/shadow:/etc/shadow:ro" \
-     $MATLAB anywave:latest
+     $MATLAB $image
