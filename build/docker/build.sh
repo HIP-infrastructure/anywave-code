1a880c319237100e06e433846c48f0dcb457388e
Bruno Colombet <bruno.colombet@univ-amu.fr>
2022-04-26 14:11:52 +0200

    minor changes

--- build/docker/build.sh
+++ build/docker/build.sh
@@ -2,5 +2,4 @@
 docker build ubuntu-aw -t ubuntu-aw:latest < ubuntu-aw/Dockerfile
 # first arg must be version number
 docker build /usr/local/ -t anywave:latest -f Dockerfile
-# build MCR version
-docker build  /usr/local/MATLAB -t anywave-mcr:latest  < anywave-mcr/Dockerfile
+
